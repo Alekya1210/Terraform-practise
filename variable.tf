56e0db7d808b36e6a04516933e8ff94805c1808d
Alekya1210 <164615321+Alekya1210@users.noreply.github.com>
2025-01-21 00:04:38 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "instancetype"{}
+variable "region"{}
+variable "availability_zone" {}
+variable "ami" {}
+variable "vpc-name" {}
+variable "igw-name" {}
+variable "subnet-name" {}
+variable "myroute_table-name" {}
+variable "myvpc-cidr_block" {}
+variable "mysub-cidr_block" {}
+variable "myroute-cidr_block" {}
+variable "my-sg-name" {}
+variable "instance-name" {}
+  
+
